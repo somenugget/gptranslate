b4767e04212621924bab4d70fd672e782c961977
Dmitriy Shcherbakan <some.nugget@gmail.com>
2023-07-29 20:47:24 +0400

    Add Stimulus

--- app/frontend/controllers/index.js
+++ app/frontend/controllers/index.js
@@ -1,11 +1,13 @@
-// Import and register all your controllers from the importmap under controllers/*
+import { Application } from "@hotwired/stimulus"
+import { registerControllers } from 'stimulus-vite-helpers'
 
-import { application } from "controllers/application"
+const application = Application.start()
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// Configure Stimulus development experience
+application.debug = true
+window.Stimulus   = application
 
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+console.log('Stimulus ⚡️ Rails')
+
+const controllers = import.meta.glob('./**/*_controller.js', { eager: true })
+registerControllers(application, controllers)
